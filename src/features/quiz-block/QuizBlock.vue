bb3094433a62d9228ea2786acb2f69116b6d3182
YuraLinkev1ch <yura.linkevich2012@gmail.com>
2023-09-06 10:04:15 +0300

    add quiz

---
+++ src/features/quiz-block/QuizBlock.vue
@@ -0,0 +1,130 @@
+<style scoped>
+.quiz-block {
+  :deep(.g-quiz) {
+    --text-color: #000000;
+    --answer-bg: #ffffff;
+    --answer-bg--hover: white;
+    --answer-bg--click: white;
+    --answer-bg--disabled: white;
+    --correct-color: transparent;
+    @apply font-medium;
+
+    & > div:nth-child(3) {
+      @apply w-full;
+    }
+
+    & .g-quiz__question {
+      @apply my-6 text-[20px] leading-6;
+      @screen lg {
+        @apply text-[24px] leading-7 mt-8;
+      }
+    }
+
+    & .g-quiz__answers-title {
+      @apply mb-4 text-[16px] leading-5 !text-black font-sans font-medium opacity-100;
+    }
+    & .g-quiz__answer-item {
+      @apply relative border-[2px] border-[#CACED9] rounded mt-2 first-of-type:mt-0 p-4 pl-[42px] text-[16px] leading-5 text-black font-medium font-sans text-left;
+
+      &::before {
+        content: "";
+        @apply absolute left-4 top-[18px] bg-transparent w-[18px] h-[18px] border-[2px] border-[#CACED9] rounded-full;
+      }
+      
+      &.picked {
+        @apply border-[#BE0238];
+        &::after {
+          content: "";
+          @apply absolute left-[19px] top-[21px] bg-[#BF0039] w-3 h-3 rounded-full;
+        }
+      }
+      &.correct {
+        @apply border-green-700;
+      }
+      &.incorrect {
+        @apply border-[#BE0238];
+        &::after {
+          content: "";
+          @apply absolute left-[19px] top-[21px] bg-[#BF0039] w-3 h-3 rounded-full;
+        }
+      }
+    }
+  }
+}
+
+.action-button {
+  @apply flex items-center justify-center mt-6 w-full h-12 p-2 bg-[#E61739] rounded text-white;
+  &:disabled {
+    @apply bg-[#F3F4F6] text-[#CFCFCF] cursor-not-allowed;
+  }
+}
+</style>
+
+<template lang="pug">
+div(class="quiz-block")
+  GQuizLight(
+    v-show="showQuizScreen",
+    :question="currentQuestion"
+    text-pick-answer-title
+    confirm-text="Проверить"
+    next-text="Продолжить"
+    textPickAnswerTitle="Выбери ответ"
+    @next="nextQuestion"
+    @answer="answerQuestion"
+  )
+    template(#action-button="{ onclick, disabled, text }")
+      button(
+        class="action-button"
+        :disabled="disabled",
+        :use-ym-track="false"
+        @click="onclick"
+      ) {{ text }}
+</template>
+
+<script setup lang="ts">
+import { GQuizLight } from "@geeckocom/activity-ui";
+const showRegistrationScreen = ref(false);
+const showQuizScreen = ref(true);
+
+const questionBeforeAnswer = {
+  id: 1,
+  text: "WireShark - это",
+  answers: [
+    {
+      id: 1,
+      text: "Программа-брандмауер",
+    },
+    {
+      id: 2,
+      text: "Программа автоматического обнаружения атак",
+    },
+    {
+      id: 3,
+      text: "Программа для анализа безопасности удаленного устройства",
+    },
+    {
+      id: 4,
+      text: "Анализатор трафика в сетях",
+    },
+  ],
+  clarification_text: null,
+  clarification_image: null,
+  is_correct: null,
+  correct_answer_id: null,
+};
+
+const currentQuestion = ref(questionBeforeAnswer);
+const nextQuestion = () => {
+  showQuizScreen.value = false;
+  showRegistrationScreen.value = true;
+};
+
+const answerQuestion = (answerId) => {
+  const correctAnswerId = 4;
+  currentQuestion.value = {
+    ...currentQuestion.value,
+    is_correct: answerId === correctAnswerId,
+    correct_answer_id: correctAnswerId,
+  };
+};
+</script>
